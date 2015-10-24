4b792030cfe417057c66b56ab2df511342073e89
Drulludanni <gudni14@ru.is>
2015-10-24 13:27:22 +0000

    fixed folder structure and added helloworld

---
+++ src/test/java/is/teamgoomba/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test 
+	public void testSomeLibraryMethod() {
+        HelloWorld test = new HelloWorld();
+        assertTrue(0, 0);
+    }
+}
