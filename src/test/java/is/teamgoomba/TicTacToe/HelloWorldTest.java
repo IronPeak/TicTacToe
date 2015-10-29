2d6bf3ae93c58393a7cd3dcc16cd22d93ccf950c
Kristinn Þorri Þrastarson <kthorri@Kristinns-MacBook-Pro.local>
2015-10-29 11:23:11 +0000

    Added test for main method

--- src/test/java/is/teamgoomba/TicTacToe/HelloWorldTest.java
+++ src/test/java/is/teamgoomba/TicTacToe/HelloWorldTest.java
@@ -3,9 +3,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
-    @Test 
+  @Test
 	public void testSomeLibraryMethod() {
-        HelloWorld test = new HelloWorld();
-        assertEquals(test.hello(), "Hello, World");
+    HelloWorld test = new HelloWorld();
+    assertEquals(test.hello(), "Hello, World");
+  }
+  @Test
+  public void testMainMethod() {
+    HelloWorld test = new HelloWorld();
+    try {
+      String[] args = new String[0];
+      test.main(args);
+    }catch(Exception e) {
+      fail("Exception caught");
     }
+  }
 }
