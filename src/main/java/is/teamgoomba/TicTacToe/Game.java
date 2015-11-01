3e3996475a2cd2e8c64e102a2aed3beeb11f5d42
Gudni <drulludanni5@gmail.com>
2015-11-01 20:47:42 +0000

    added tests for Move() and implemented it, game is now in a working state such that a webui can be implemented for it but a few features are still missing from final version

--- src/main/java/is/teamgoomba/TicTacToe/Game.java
+++ src/main/java/is/teamgoomba/TicTacToe/Game.java
@@ -3,15 +3,33 @@ package is.teamgoomba.TicTacToe;
 
 public class Game {
     Board board;
-    int playertomove;
-
+    int player;
+    private final int dim = 3;
     Game(){
         board = new Board();
-        playertomove = 1;
+        player = 1;
     }
 
-    String GetBoard(){
+    public String GetBoard(){
         return board.GetBoard();
     }
+
+    public boolean Move(int inp){
+        if(inp < 0 || inp > 8){
+	    return false;
+	} 
+        int x = inp % dim;
+        int y = inp / dim;
+        boolean success = board.Place(x,y,player);
+        if(success){
+            if(player == 1){
+                player = 2;
+                return true;
+            }
+            player = 1;
+            return true;
+        }
+        return false;
+    }    
 }
 
--- src/test/java/is/teamgoomba/TicTacToe/GameTest.java
+++ src/test/java/is/teamgoomba/TicTacToe/GameTest.java
@@ -1,4 +1,3 @@
-p
 package is.teamgoomba.TicTacToe;
 
 import org.junit.Test;
@@ -8,8 +7,45 @@ public class GameTest {
 
   @Test	
   public void testGetBoard() {
-     Game game = new Game();
-     assertEquals(game.GetBoard(),"000000000");
+      Game game = new Game();
+      assertEquals(game.GetBoard(),"000000000");
   }
- 
+  @Test	
+  public void testMoveAllSquares(){
+      Game game = new Game();
+      assertTrue(game.Move(0));
+      assertTrue(game.Move(1));
+      assertTrue(game.Move(2));
+      assertTrue(game.Move(3));
+      assertTrue(game.Move(4));
+      assertTrue(game.Move(5));
+      assertTrue(game.Move(6));
+      assertTrue(game.Move(7));
+      assertTrue(game.Move(8));
+      assertEquals(game.GetBoard(),"121212121");
+  }
+
+  @Test	
+  public void testInvalidMove(){ 
+      Game game = new Game();
+      assertTrue(game.Move(0));
+      assertFalse(game.Move(0)); 
+      assertTrue(game.Move(1));
+      assertFalse(game.Move(1));
+      assertTrue(game.Move(2));
+      assertFalse(game.Move(2));
+      assertTrue(game.Move(3));
+      assertFalse(game.Move(0));
+      assertEquals(game.GetBoard(),"121200000");
+  } 
+  @Test	
+  public void testMoveOutOfBounds(){ 
+      Game game = new Game();
+      assertFalse(game.Move(-1)); 
+      assertFalse(game.Move(9));
+      assertFalse(game.Move(23123));
+      assertFalse(game.Move(-2222));
+      assertTrue(game.Move(3));
+      assertEquals(game.GetBoard(),"000100000");
+  }  
 }
