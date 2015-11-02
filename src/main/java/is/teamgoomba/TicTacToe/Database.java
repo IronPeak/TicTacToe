742efc803fd89d111c81b2f317734125a84acc4e
IronPeak <Hrafnh14@gmail.com>
2015-11-02 18:06:23 +0000

    Created a basic database class

--- src/main/java/is/teamgoomba/TicTacToe/Database.java
+++ src/main/java/is/teamgoomba/TicTacToe/Database.java
@@ -1,5 +1,38 @@
 package is.teamgoomba.TicTacToe;
 
-public class Database {
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Database {
+	private Connection connection;
+	public Database() throws URISyntaxException, SQLException {
+		connection = getConnection();
+	}
+	
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
+	public int getXWins() throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT NumberOfXWins FROM generalinfo LIMIT 1;");
+		return rs.getInt("NumberOfXWins");
+	}
+	
+	public int getOWins() throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT NumberOfOWins FROM generalinfo LIMIT 1;");
+		return rs.getInt("NumberOfOWins");
+	}
 }
