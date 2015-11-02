83c7df42ae473c42c0b34818c131239798e66ef4
IronPeak <Hrafnh14@gmail.com>
2015-11-02 18:22:50 +0000

    Database class

--- src/main/java/is/teamgoomba/TicTacToe/Database.java
+++ src/main/java/is/teamgoomba/TicTacToe/Database.java
@@ -9,9 +9,18 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Database {
-	private Connection connection;
+private Connection connection;
 	public Database() throws URISyntaxException, SQLException {
 		connection = getConnection();
+		setupRow();
+	}
+	
+	private void setupRow() throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM generalinfo;");
+		if (!rs.next()){
+			stmt.executeQuery("INSERT INTO generalinfo VALUES (0, 0, 0, 0);");
+		}
 	}
 	
 	private static Connection getConnection() throws URISyntaxException, SQLException {
@@ -24,6 +33,12 @@ public class Database {
 		return DriverManager.getConnection(dbUrl, username, password);
 	}
 	
+	public int getVisits() throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT NumberOfVisits FROM generalinfo LIMIT 1;");
+		return rs.getInt("NumberOfVisits");
+	}
+	
 	public int getXWins() throws SQLException {
 		Statement stmt = connection.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT NumberOfXWins FROM generalinfo LIMIT 1;");
@@ -34,5 +49,31 @@ public class Database {
 		Statement stmt = connection.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT NumberOfOWins FROM generalinfo LIMIT 1;");
 		return rs.getInt("NumberOfOWins");
+	}
+	
+	public int getTies() throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT NumberOfTies FROM generalinfo LIMIT 1;");
+		return rs.getInt("NumberOfTies");
+	}
+	
+	public void incVisits() throws SQLException {
+		Statement stmt = connection.createStatement();
+		stmt.executeQuery("UPDATE generalinfo SET NumberOfVisists = NumberOfVisits + 1;");
+	}
+	
+	public void incXWins() throws SQLException {
+		Statement stmt = connection.createStatement();
+		stmt.executeQuery("UPDATE generalinfo SET NumberOfXWins = NumberOfXWins + 1;");
+	}
+	
+	public void incOWins() throws SQLException {
+		Statement stmt = connection.createStatement();
+		stmt.executeQuery("UPDATE generalinfo SET NumberOfOWins = NumberOfOWins + 1;");
+	}
+	
+	public void incTies() throws SQLException {
+		Statement stmt = connection.createStatement();
+		stmt.executeQuery("UPDATE generalinfo SET NumberOfTies = NumberOfTies + 1;");
 	}
 }
