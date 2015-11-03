7dbdf0973ed435cb607b789176ccace91e32baab
IronPeak <Hrafnh14@gmail.com>
2015-11-03 17:51:57 +0000

    Selenium tests

--- src/test/java/is/teamgoomba/TicTacToe/Selenium/SeleniumTest.java
+++ src/test/java/is/teamgoomba/TicTacToe/Selenium/SeleniumTest.java
@@ -28,15 +28,9 @@ public class SeleniumTest {
 	}
 	
 	@Test
-	public void testClearBoard() {
+	public void testClearBoard() throws Exception {
 		driver.get(baseUrl + "");
-		WebElement newGameButton = driver.findElement(By.id("restart"));
-		newGameButton.click();
-		try {
-			Thread.sleep(4000);
-		} catch(Exception e) {
-			
-		}
+		resetBoard();
 		for(int i = 0; i < 9; i++) {
 			WebElement box = driver.findElement(By.id("box_" + i));
 			String classAttribute = box.getAttribute("class");
@@ -45,39 +39,24 @@ public class SeleniumTest {
 	}
 	
 	@Test
-	public void testStartPlayerChanges() {
+	public void testStartPlayerChanges() throws Exception {
 		driver.get(baseUrl + "");
-		try {
-			Thread.sleep(4000);
-		} catch(Exception e) {
-			
-		}
-		WebElement player = driver.findElement(By.id("player"));
-		WebElement newGameButton = driver.findElement(By.id("restart"));
-		String expectedvalue;
-		if(player.getText().equals("1")) {
-			expectedvalue = "2";
-		} else {
-			expectedvalue = "1";
-		}
-		newGameButton.click();
-		try {
-			Thread.sleep(4000);
-			WebDriverWait wait = new WebDriverWait(driver, 10);
-			wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("player"), expectedvalue));
-		} catch(Exception e) {
-			fail("Player did not change!" + expectedvalue);
-		}
+		Thread.sleep(4000);
+		String player1 = driver.findElement(By.id("player")).getText();
+		resetBoard();
+		Thread.sleep(4000);
+		String player2 = driver.findElement(By.id("player")).getText();
+		assertTrue(player1.equals(player2));
 	}
 	
 	@Test
-	public void testClickBox() {
+	public void testClickBox() throws Exception {
 		driver.get(baseUrl + "");
-		try {
-			Thread.sleep(4000);
-		} catch(Exception e) {
-			
-		}
+		Thread.sleep(4000);
+		driver.get(baseUrl + "");
+		WebElement newGameButton = driver.findElement(By.id("restart"));
+		newGameButton.click();
+		Thread.sleep(4000);
 		WebElement player = driver.findElement(By.id("player"));
 		WebElement box0 = driver.findElement(By.id("box_0"));
 		String expectedvalue;
@@ -93,6 +72,56 @@ public class SeleniumTest {
 		} catch(Exception e) {
 			fail("Could not click box!");
 		}
+	}
+	
+	@Test
+	public void testMultipleClicksBox() throws Exception {
+		driver.get(baseUrl + "");
+		resetBoard();
+		WebElement player = driver.findElement(By.id("player"));
+		WebElement box0 = driver.findElement(By.id("box_0"));
+		String expectedvalue;
+		if(player.getText().equals("1")) {
+			expectedvalue = "button_box player-X";
+		} else {
+			expectedvalue = "button_box player-O";
+		}
+		try {
+			box0.click();
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(classIs(By.id("box_0"), expectedvalue));
+			driver.findElement(By.id("box_0")).click();
+			wait.until(classIs(By.id("box_0"), expectedvalue));
+		} catch(Exception e) {
+			fail("Could not click box!");
+		}
+	}
+	
+	@Test
+	public void testVictory() throws Exception {
+		driver.get(baseUrl + "");
+		resetBoard();
+		WebElement player = driver.findElement(By.id("player"));
+		WebElement box0 = driver.findElement(By.id("box_0"));
+		String expectedvalue = "player " + player.getText() + " is the winner!";
+		click(By.id("box_0"));
+		click(By.id("box_1"));
+		click(By.id("box_4"));
+		click(By.id("box_2"));
+		click(By.id("box_8"));
+		String actual = driver.findElement(By.id("winner")).getText();
+		assertTrue(expectedvalue.equals(actual));
+	}
+	
+	private void resetBoard() throws Exception {
+		WebElement newGameButton = driver.findElement(By.id("restart"));
+		newGameButton.click();
+		Thread.sleep(4000);
+	}
+	
+	private void click(final By locator) throws Exception {
+		driver.findElement(locator).click();
+		Thread.sleep(4000);
 	}
 	
 	public static ExpectedCondition<Boolean> classIs(final By locator, final String classname) {
